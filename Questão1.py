f725565e0613e3d92219a9b84c233ebeee931b7b
Dilberto <dilbertoms@gmail.com>
2023-03-23 00:12:04 -0300

    Initial commit

---
+++ Questão1.py
@@ -0,0 +1,10 @@
+#Solicite ao usuário um valor numérico, inteiro ou real, e escrever se é positivo ou negativo (considere
+#o valor zero como positivo).
+
+
+
+n1=float (input("Digite um numero"))
+if n1>=0:
+    print(f"Seu valor  {n1} é positivo")
+else:
+    print(f"Seu valor {n1} é negativo ")
